0d02b00251fdd40fee4fc8828fe1068ae8a974ae
anwebdevelopers <anwebdev78@gmail.com>
2019-02-07 18:08:29 +0400

    7.02.2019

--- app/js/common.js
+++ app/js/common.js
@@ -2,74 +2,99 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
 
     'use strict';
 
-    const $window = $( window );
+
 
     /*******************************************************/
-    //WINDOW HORISONTAL RESIZE
+    //MENU
     /*******************************************************/
-    // $window.on( 'resize', function() {
-    //     const newWindowWidth = $window.width();
-    //
-    //     if ( windowWidth !== newWindowWidth ) {
-    //
-    //         windowWidth = newWindowWidth;
-    //
-    //         //MENU MOBILE
-    //         $menu.removeClass( 'active' );
-    //         $buttonMenu.removeClass( 'active' );
-    //
-    //         //SIDE NAV MOBILE ACCORDION
-    //         $accordion.removeClass( 'active' );
-    //     }
-    // });
+    ( function() {
+
+        let windowWidth = window.innerWidth;
+
+        const elemNav = document.querySelector('.nav'),
+            buttonNav = document.querySelector('.header__nav-button');
+
+        buttonNav.addEventListener( 'click', function( e ) {
+            e.stopPropagation();
+            if ( buttonNav.classList.contains( 'active' ) ) {
+                buttonNav.classList.remove('active');
+                // elemNav.style.display = '';
+                elemNav.classList.remove('active');
+            } else {
+                buttonNav.classList.add( 'active' );
+                // elemNav.style.display = 'block';
+                elemNav.classList.add('active');
+            }
+        } );
+
+        window.addEventListener( 'resize', function() {
+
+            const newWindowWidth = window.innerWidth;
+
+            if ( windowWidth !== newWindowWidth ) {
+
+                windowWidth = newWindowWidth;
+
+                buttonNav.classList.remove('active');
+                // elemNav.style.display = '';
+                elemNav.classList.remove('active');
+            }
+        } );
+
+    } () );
+
 
     //*********************************************************//
     //Lazy load for images
     //*********************************************************//
-    const lazyLoad = new IntersectionObserver(
+    ( function() {
 
-        function( entries ) {
+        const lazyLoadImg = new IntersectionObserver(
 
-            for ( let i = 0; i < entries.length; i++  ) {
+            function( entries ) {
 
-                const entry = entries[ i ];
-                const target = entry.target;
+                for ( let i = 0; i < entries.length; i++  ) {
 
-                if ( entry.isIntersecting && target.hasAttribute( 'data-lazy-load' ) ) {
+                    const entry = entries[ i ];
+                    const target = entry.target;
 
-                    if ( target.nodeName === 'IMG' ) {
-                        target.setAttribute( 'src', target.getAttribute( 'data-lazy-load' ) );
-                    } else if ( target.nodeName === 'SOURCE' ) {
-                        target.setAttribute( 'srcset', target.getAttribute( 'data-lazy-load' ) );
-                    } else {
-                        target.style.backgroundImage = 'url(' + target.getAttribute( 'data-lazy-load' ) + ')';
+                    if ( entry.isIntersecting && target.hasAttribute( 'data-lazy-load' ) ) {
+
+                        if ( target.nodeName === 'IMG' ) {
+                            target.setAttribute( 'src', target.getAttribute( 'data-lazy-load' ) );
+                        } else if ( target.nodeName === 'SOURCE' ) {
+                            target.setAttribute( 'srcset', target.getAttribute( 'data-lazy-load' ) );
+                        } else {
+                            target.style.backgroundImage = 'url(' + target.getAttribute( 'data-lazy-load' ) + ')';
+                        }
+
+                        target.removeAttribute( 'data-lazy-load' )
+
+                        lazyLoadImg.unobserve( target );
+
+                        target.style.opacity = 1;
                     }
-
-                    target.removeAttribute( 'data-lazy-load' )
-
-                    lazyLoad.unobserve( target );
-
-                    target.style.opacity = 1;
                 }
+            },
+            {
+                root: null,
+                rootMargin: ( window.innerHeight / 2 ) + 'px ' + ( window.innerWidth / 2 ) + 'px',
+                threshold: [ 0 ],
             }
-        },
-        {
-            root: null,
-            rootMargin: ( window.innerHeight / 2 ) + 'px ' + ( window.innerWidth / 2 ) + 'px',
-            threshold: [ 0 ],
+        );
+
+        // Start observing an element
+        const lazyLoadImgElems = document.querySelectorAll( '[ data-lazy-load ]' );
+
+        for ( let i = 0; i < lazyLoadImgElems.length; i++  ) {
+
+            lazyLoadImg.observe( lazyLoadImgElems[ i ] );
+
+            lazyLoadImgElems[ i ].style.opacity = 0;
+            lazyLoadImgElems[ i ].style.transition = '1s';
         }
-    );
-
-    // Start observing an element
-    const lazyLoadElems = document.querySelectorAll( '[ data-lazy-load ]' );
-
-    for ( let i = 0; i < lazyLoadElems.length; i++  ) {
-
-        lazyLoad.observe( lazyLoadElems[ i ] );
-
-        lazyLoadElems[ i ].style.opacity = 0;
-        lazyLoadElems[ i ].style.transition = '1s';
-    }
+        
+    } () );
 
     //*********************************************************//
     //SLIDER ABOUT
@@ -99,52 +124,88 @@ document.addEventListener( 'DOMContentLoaded', function( event ) {
     //*********************************************************//
     //YANDEX MAP
     //*********************************************************//
-    if ( ( typeof ymaps === 'object' ) && $( '#map' ).length ) {
+    ( function() {
 
-        ymaps.ready( function() {
+        const mapElem = document.querySelector( '#map' );
 
-            const myMap = new ymaps.Map( 'map', {
-                center: [ 55.769833, 37.426492 ],
-                zoom: 16,
-                controls: [],
-                behaviors: [ 'drag', 'dblClickZoom', 'rightMouseButtonMagnifier', 'multiTouch' ]
-            }, {
-                searchControlProvider: 'yandex#search'
-            });
+        const lazyLoadMap = new IntersectionObserver(
 
-            //Элементы управления
-            myMap.controls.add( 'zoomControl', {
-                size: 'small',
-                position: {
-                    top: 'auto',
-                    right: 10,
-                    bottom: 50
+            function( entries ) {
+
+                for ( let i = 0; i < entries.length; i++  ) {
+
+                    const entry = entries[ i ];
+                    const target = entry.target;
+
+                    if ( entry.isIntersecting && ( typeof ymaps === 'object' ) ) {
+
+                        ymaps.ready( function() {
+
+                            const myMap = new ymaps.Map( 'map', {
+                                center: [ 55.769833, 37.426492 ],
+                                zoom: 16,
+                                controls: [],
+                                behaviors: [ 'drag', 'dblClickZoom', 'rightMouseButtonMagnifier', 'multiTouch' ]
+                            }, {
+                                searchControlProvider: 'yandex#search'
+                            });
+
+                            //Элементы управления
+                            myMap.controls.add( 'zoomControl', {
+                                size: 'small',
+                                position: {
+                                    top: 'auto',
+                                    right: 10,
+                                    bottom: 50
+                                }
+                            } );
+
+                            myMap.geoObjects.add( new ymaps.Placemark(
+                                [ 55.769833, 37.426492 ],
+                                {
+                                    hintContent: 'Адрес: г. Москва, ул. Крылатская, д.15',
+                                    balloonContent: 'Адрес: г. Москва, ул. Крылатская, д.15',
+                                },
+                                {
+                                    iconLayout: 'default#image',
+                                    iconImageHref: 'img/icon-mark.svg',
+                                    iconImageSize: [ 53, 62 ],
+                                    iconImageOffset: [- 26, -62 ],
+                                }
+                            ) );
+
+                            //Вкл/Выкл драг карты при адаптиве
+                            const manageDrag = function() {
+                                window.innerWidth <= 992 ? myMap.behaviors.disable( 'drag' ) : myMap.behaviors.enable( 'drag' )
+                            };
+                            window.onload = manageDrag
+                            window.onresize = manageDrag
+
+                            //перерисуем карту по ресайзу
+                            typeof ResizeObserver === 'object' && new ResizeObserver( function( entries ) {
+                                myMap.container.fitToViewport()
+                            } ).observe( mapElem );
+
+                            //перерисуем карту после инициализации
+                            myMap.container.fitToViewport();
+
+                        } );
+
+                        lazyLoadMap.unobserve( target );
+                    }
                 }
-            } );
+            },
+            {
+                root: null,
+                rootMargin: ( window.innerHeight / 2 ) + 'px ' + ( window.innerWidth / 2 ) + 'px',
+                threshold: [ 0 ],
+            }
+        );
 
-            myMap.geoObjects.add( new ymaps.Placemark(
-                [ 55.769833, 37.426492 ],
-                {
-                    hintContent: 'Адрес: г. Москва, ул. Крылатская, д.15',
-                    balloonContent: 'Адрес: г. Москва, ул. Крылатская, д.15',
-                },
-                {
-                    iconLayout: 'default#image',
-                    iconImageHref: 'img/icon-mark.svg',
-                    iconImageSize: [ 53, 62 ],
-                    iconImageOffset: [- 26, -62 ],
-                }
-            ) );
+        // Start observing an element
+        lazyLoadMap.observe( mapElem );
 
-            //Вкл/Выкл драг карты при адаптиве
-            $window.on( 'resize load', function() {
-                $window.width() <= 992 ? myMap.behaviors.disable( 'drag' ) : myMap.behaviors.enable('drag')
-            });
+    } () );
 
-            //перерисуем карту после инициализации
-            myMap.container.fitToViewport();
-
-        });
-    }
 
 } );
